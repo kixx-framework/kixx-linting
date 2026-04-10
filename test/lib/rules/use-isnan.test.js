402d1c13a45ae57a89d90e170838d1c794532b5e
Kris Walker <kris@kixx.name>
2026-04-10 15:57:12 -0400

    Ignore some lint rules in our tests
     Changes to be committed:
    	modified:   test/lib/rules/use-isnan.test.js
    	modified:   test/lib/rules/valid-typeof.test.js

--- test/lib/rules/use-isnan.test.js
+++ test/lib/rules/use-isnan.test.js
@@ -248,62 +248,6 @@ const valid = [
     },
 ];
 
-describe("use-isnan", ({ describe }) => {
-
-    const globalRules = { "use-isnan": ["error"] };
-    const defaultLanguageOptions = { sourceType: "script" };
-
-    describe("valid code", ({ it }) => {
-        it("has expected outcomes", () => {
-            valid.forEach(({ text, code, options, languageOptions }, i) => {
-                const sourceText = text ?? code;
-                const file = { text: sourceText };
-
-                let rules = globalRules;
-                if (options) {
-                    rules = structuredClone(globalRules);
-                    rules["use-isnan"] = rules["use-isnan"].concat(options);
-                }
-
-                const res = lintText(file, rules, { ...defaultLanguageOptions, ...languageOptions });
-
-                if (res.errorCount > 0 || res.warningCount > 0) {
-                    // eslint-disable-next-line no-console
-                    console.error(res);
-                }
-
-                assertEqual(0, res.errorCount, `errorCount:[${i}]:${sourceText.slice(0, 52)} ...`);
-                assertEqual(0, res.warningCount, `warningCount:[${i}]:${sourceText.slice(0, 52)} ...`);
-            });
-        });
-    });
-
-    describe("invalid code", ({ it }) => {
-        it("has expected outcomes", () => {
-            invalid.forEach(({ text, code, options, languageOptions, errors = 1 }, i) => {
-                const sourceText = text ?? code;
-                const file = { text: sourceText };
-
-                let rules = globalRules;
-                if (options) {
-                    rules = structuredClone(globalRules);
-                    rules["use-isnan"] = rules["use-isnan"].concat(options);
-                }
-
-                const res = lintText(file, rules, { ...defaultLanguageOptions, ...languageOptions });
-
-                assertEqual(errors, res.errorCount, `errorCount:[${i}]:${sourceText.slice(0, 52)} ...`);
-                assertEqual(0, res.warningCount, `warningCount:[${i}]:${sourceText.slice(0, 52)} ...`);
-
-                res.messages.forEach((message) => {
-                    assertEqual("use-isnan", message.ruleId, `message.ruleId:[${i}]:${sourceText.slice(0, 52)} ...`);
-                    assertNonEmptyString(message.message, `message.message:[${i}]:${sourceText.slice(0, 52)} ...`);
-                });
-            });
-        });
-    });
-});
-
 const invalid = [
     {
         text: "123 == NaN;",
@@ -579,3 +523,59 @@ const invalid = [
         languageOptions: { ecmaVersion: 2020 },
     },
 ];
+
+describe("use-isnan", ({ describe }) => {
+
+    const globalRules = { "use-isnan": ["error"] };
+    const defaultLanguageOptions = { sourceType: "script" };
+
+    describe("valid code", ({ it }) => {
+        it("has expected outcomes", () => {
+            valid.forEach(({ text, code, options, languageOptions }, i) => {
+                const sourceText = text ?? code;
+                const file = { text: sourceText };
+
+                let rules = globalRules;
+                if (options) {
+                    rules = structuredClone(globalRules);
+                    rules["use-isnan"] = rules["use-isnan"].concat(options);
+                }
+
+                const res = lintText(file, rules, { ...defaultLanguageOptions, ...languageOptions });
+
+                if (res.errorCount > 0 || res.warningCount > 0) {
+                    // eslint-disable-next-line no-console
+                    console.error(res);
+                }
+
+                assertEqual(0, res.errorCount, `errorCount:[${i}]:${sourceText.slice(0, 52)} ...`);
+                assertEqual(0, res.warningCount, `warningCount:[${i}]:${sourceText.slice(0, 52)} ...`);
+            });
+        });
+    });
+
+    describe("invalid code", ({ it }) => {
+        it("has expected outcomes", () => {
+            invalid.forEach(({ text, code, options, languageOptions, errors = 1 }, i) => {
+                const sourceText = text ?? code;
+                const file = { text: sourceText };
+
+                let rules = globalRules;
+                if (options) {
+                    rules = structuredClone(globalRules);
+                    rules["use-isnan"] = rules["use-isnan"].concat(options);
+                }
+
+                const res = lintText(file, rules, { ...defaultLanguageOptions, ...languageOptions });
+
+                assertEqual(errors, res.errorCount, `errorCount:[${i}]:${sourceText.slice(0, 52)} ...`);
+                assertEqual(0, res.warningCount, `warningCount:[${i}]:${sourceText.slice(0, 52)} ...`);
+
+                res.messages.forEach((message) => {
+                    assertEqual("use-isnan", message.ruleId, `message.ruleId:[${i}]:${sourceText.slice(0, 52)} ...`);
+                    assertNonEmptyString(message.message, `message.message:[${i}]:${sourceText.slice(0, 52)} ...`);
+                });
+            });
+        });
+    });
+});
--- test/lib/rules/valid-typeof.test.js
+++ test/lib/rules/valid-typeof.test.js
@@ -88,62 +88,6 @@ const valid = [
     },
 ];
 
-describe("valid-typeof", ({ describe }) => {
-
-    const globalRules = { "valid-typeof": ["error"] };
-    const defaultLanguageOptions = { sourceType: "script" };
-
-    describe("valid code", ({ it }) => {
-        it("has expected outcomes", () => {
-            valid.forEach(({ text, code, options, languageOptions }, i) => {
-                const sourceText = text ?? code;
-                const file = { text: sourceText };
-
-                let rules = globalRules;
-                if (options) {
-                    rules = structuredClone(globalRules);
-                    rules["valid-typeof"] = rules["valid-typeof"].concat(options);
-                }
-
-                const res = lintText(file, rules, { ...defaultLanguageOptions, ...languageOptions });
-
-                if (res.errorCount > 0 || res.warningCount > 0) {
-                    // eslint-disable-next-line no-console
-                    console.error(res);
-                }
-
-                assertEqual(0, res.errorCount, `errorCount:[${i}]:${sourceText.slice(0, 52)} ...`);
-                assertEqual(0, res.warningCount, `warningCount:[${i}]:${sourceText.slice(0, 52)} ...`);
-            });
-        });
-    });
-
-    describe("invalid code", ({ it }) => {
-        it("has expected outcomes", () => {
-            invalid.forEach(({ text, code, options, languageOptions, errors = 1 }, i) => {
-                const sourceText = text ?? code;
-                const file = { text: sourceText };
-
-                let rules = globalRules;
-                if (options) {
-                    rules = structuredClone(globalRules);
-                    rules["valid-typeof"] = rules["valid-typeof"].concat(options);
-                }
-
-                const res = lintText(file, rules, { ...defaultLanguageOptions, ...languageOptions });
-
-                assertEqual(errors, res.errorCount, `errorCount:[${i}]:${sourceText.slice(0, 52)} ...`);
-                assertEqual(0, res.warningCount, `warningCount:[${i}]:${sourceText.slice(0, 52)} ...`);
-
-                res.messages.forEach((message) => {
-                    assertEqual("valid-typeof", message.ruleId, `message.ruleId:[${i}]:${sourceText.slice(0, 52)} ...`);
-                    assertNonEmptyString(message.message, `message.message:[${i}]:${sourceText.slice(0, 52)} ...`);
-                });
-            });
-        });
-    });
-});
-
 const invalid = [
     {
         text: "typeof foo === 'strnig'",
@@ -222,3 +166,59 @@ const invalid = [
         languageOptions: { ecmaVersion: 6 },
     },
 ];
+
+describe("valid-typeof", ({ describe }) => {
+
+    const globalRules = { "valid-typeof": ["error"] };
+    const defaultLanguageOptions = { sourceType: "script" };
+
+    describe("valid code", ({ it }) => {
+        it("has expected outcomes", () => {
+            valid.forEach(({ text, code, options, languageOptions }, i) => {
+                const sourceText = text ?? code;
+                const file = { text: sourceText };
+
+                let rules = globalRules;
+                if (options) {
+                    rules = structuredClone(globalRules);
+                    rules["valid-typeof"] = rules["valid-typeof"].concat(options);
+                }
+
+                const res = lintText(file, rules, { ...defaultLanguageOptions, ...languageOptions });
+
+                if (res.errorCount > 0 || res.warningCount > 0) {
+                    // eslint-disable-next-line no-console
+                    console.error(res);
+                }
+
+                assertEqual(0, res.errorCount, `errorCount:[${i}]:${sourceText.slice(0, 52)} ...`);
+                assertEqual(0, res.warningCount, `warningCount:[${i}]:${sourceText.slice(0, 52)} ...`);
+            });
+        });
+    });
+
+    describe("invalid code", ({ it }) => {
+        it("has expected outcomes", () => {
+            invalid.forEach(({ text, code, options, languageOptions, errors = 1 }, i) => {
+                const sourceText = text ?? code;
+                const file = { text: sourceText };
+
+                let rules = globalRules;
+                if (options) {
+                    rules = structuredClone(globalRules);
+                    rules["valid-typeof"] = rules["valid-typeof"].concat(options);
+                }
+
+                const res = lintText(file, rules, { ...defaultLanguageOptions, ...languageOptions });
+
+                assertEqual(errors, res.errorCount, `errorCount:[${i}]:${sourceText.slice(0, 52)} ...`);
+                assertEqual(0, res.warningCount, `warningCount:[${i}]:${sourceText.slice(0, 52)} ...`);
+
+                res.messages.forEach((message) => {
+                    assertEqual("valid-typeof", message.ruleId, `message.ruleId:[${i}]:${sourceText.slice(0, 52)} ...`);
+                    assertNonEmptyString(message.message, `message.message:[${i}]:${sourceText.slice(0, 52)} ...`);
+                });
+            });
+        });
+    });
+});
